4b2058a308cfdb22fe96dfb24269fdbd2972338c
sebastian <sebastianalexis869@gmail.com>
2024-05-03 23:31:49 -0400

    setCapcode

--- src/pages/FormLogin/FormLogin.tsx
+++ src/pages/FormLogin/FormLogin.tsx
@@ -238,6 +238,7 @@ export const FormLogin = () => {
         onHide={() => {
           setCapCode("");
           setShowCapCode(false);
+          setInvalidCapcode(false)
         }}>
         <InputOtp
           length={4}
